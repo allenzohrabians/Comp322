4aa43ff0257cbc05b30f6f67b655500e3273b9b0
allenzohrabians <60366684+allenzohrabians@users.noreply.github.com>
2020-02-15 22:44:22 -0800

    Update mainlab0.c

--- mainlab0.c
+++ mainlab0.c
@@ -2,20 +2,116 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char** argv)
 {
-  int binaryVal;
-  char asciiVal;
-  int decimalVal;
-  bool evenVal;
+  int reader;
+  int binaryVal[8] = {0};
 
-   FILE *fptr;
-   if ((fptr = fopen("C:\\Users\allen\Desktop\Comp322\testdata.txt","r")) == NULL){
-       printf("Error! opening file");
-       // Program exits if the file pointer returns NULL.
-       exit(1);
-   }
+  if (argc < 2)
+  {
+    printf("Sorry, file does not have a valid name.\n");
+  }
 
-   fscanf(fptr,"%d", &binaryVal);
-   printf("Value of n=%d", binaryVal);
- }
+  else
+  {
+    printf("Original ASCII    Decimal  Parity\n");
+    printf("-------- -------- -------- --------\n");
+
+    FILE* file = fopen(argv[1], "r");
+
+    int argCounter = 0;
+
+    while((reader = fgetc(file)) != EOF)
+    {
+      if (reader != ' ')
+      {
+        binaryVal[argCounter] = reader;
+        argCounter++;
+      }
+
+      if (argCounter == 8 || (argCounter > 0 && reader == " "))
+      {
+        for (int i = 0; i < 8; i++)
+        {
+          printf("%c", binaryVal[i]);
+        }
+        printf("\t");
+
+        printf("%c", decimalConversion(binaryVal));
+
+        printf("%8d\t", decimalConversion(binaryVal));
+
+        printf("%s", parity(binaryVal));
+
+        for (int i = 0; i < 8; i++)
+        {
+          binaryVal[i] = '0';
+        }
+
+        argCounter = 0;
+
+        printf("\n");
+      }
+    }
+
+    if (argCounter > 0)
+    {
+      for (int j = counter - 1; j < 8; j++)
+      {
+        binaryVal[j] = '0';
+      }
+
+      for (int i = 0; i < 8; i++)
+      {
+        printf("%c", binaryVal[i]);
+      }
+      printf("\t");
+
+      printf("%c", decimalConversion(binaryVal));
+
+      printf("%8d\t", decimalConversion(binaryVal));
+
+      printf("%s", parity(binaryVal));
+
+      printf("\n");
+
+      argCounter = 0;
+    }
+
+    fclose(file);
+  }
+
+  return 0;
+}
+
+decimalConversion(binaryArr[])
+{
+  int decimalVal = 0;
+
+  for (int i = 7, z = 0; i > 0; i--, j++)
+  {
+    decimalVal = decimalVal + ((binaryArr[i] - 48) * pow(2,z));
+  }
+
+  return decimalVal;
+}
+
+char* parity(binaryArr[])
+{
+  int sumPar = 0;
+
+  for (int i = 0; i < 8; i++)
+  {
+    sumPar += binaryArr[i];
+  }
+
+  if(sumPar % 2 == 0)
+  {
+    return "EVEN";
+  }
+
+  else
+  {
+    return "ODD";
+  }
+}
