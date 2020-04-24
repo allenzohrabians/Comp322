4dae712bce60c6a9737376776da031b5436d025a
allenzohrabians <60366684+allenzohrabians@users.noreply.github.com>
2020-04-24 06:33:34 -0700

    Update catcher.c

--- Lab 3/catcher.c
+++ Lab 3/catcher.c
@@ -1,13 +1,15 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/types.h>
+#include <string.h> /* strcmp */
+#include <stdlib.h> /* exit */
+#include <stdio.h>  /* printf, stderr, fprintf */
+#include <signal.h> /* signal */
+#include <unistd.h> /* pause */
+#include <errno.h>  /* errno */
+#include <time.h>  /* time */
+#include <sys/types.h>  /* pid_t */
 
-typedef void (*sighandler_t)(int);
+typedef void (*sighandler_t)(int); // pointer-to-function type
+
+// separate arrays for the signals themselves as well as the corresponding strings of the signal names
 
 char signalStrings[27][10] = {"HUP", "INT", "QUIT", "ILL", "TRAP", "ABRT", "IOT", "BUS", "FPE", "USR1", "SEGV", "USR2", "PIPE", "ALRM", "TERM", "STKFLT", "CHLD", "CONT", "TSTP", "TTIN", "TTOU","URG", "XCPU", "XFSZ", "VTALRM", "PROF", "WINCH"};
 int signalInts[27] = {SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGIOT, SIGBUS, SIGFPE, SIGUSR1, SIGSEGV, SIGUSR2, SIGPIPE, SIGALRM, SIGTERM, SIGSTKFLT, SIGCHLD, SIGCONT, SIGTSTP, SIGTTIN, SIGTTOU, SIGURG, SIGXCPU, SIGXFSZ, SIGVTALRM, SIGPROF, SIGWINCH};
@@ -25,17 +27,17 @@ void handler(int sig)
     if(sig == signalInts[i])
     {
 
-      printf("SIG%s caught at %ld\n", signalStrings[i], seconds);
+      printf("SIG%s caught at %ld\n", signalStrings[i], seconds);   // prints what signal was caught and at what time
 
-      allSignalCounter++;
+      allSignalCounter++;   // total number of signals caught for final print statement
 
-      if(i == 14)
+      if(i == 14) // index for SIGTERM
       {
-        sigtermCounter++;
+        sigtermCounter++; // increments after each successive SIGTERM signal
       }
       else
       {
-        sigtermCounter = 0;
+        sigtermCounter = 0; // resets to 0 if SIGTERM is not the signal caught
       }
 
       break;
@@ -43,13 +45,13 @@ void handler(int sig)
     i++;
   }
 
-  signal(sig, handler);
+  signal(sig, handler); // the handler registers itself again
 }
 
 int main(int argc, char* argv[])
 {
   pid_t process_id;
-  process_id = getpid();
+  process_id = getpid(); // Retrieval and printing of PID
   fprintf(stderr,"catcher: $$ = %d\n", process_id);
   void handler(int);
   int i = 1;
@@ -58,9 +60,9 @@ int main(int argc, char* argv[])
     int j = 0;
     while(j < 27)
     {
-      if(strcmp(argv[i], signalStrings[j]) == 0)
+      if(strcmp(argv[i], signalStrings[j]) == 0)  // indicates that the two strings are equal
       {
-        signal(signalInts[j], handler);
+        signal(signalInts[j], handler);         // registers a handler for each argument
         break;
       }
       j++;
@@ -70,7 +72,7 @@ int main(int argc, char* argv[])
 
   while(sigtermCounter != 3)
   {
-    pause();
+    pause();  // the program pauses itself continually and terminates once SIGTERM counter = 3
   }
 
   fprintf(stderr, "catcher: Total signals count = %d\n", allSignalCounter);
