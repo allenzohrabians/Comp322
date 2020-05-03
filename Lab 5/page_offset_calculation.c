ebb97e49c6c9c2c8dc188c723982d87ab1005ed6
allenzohrabians <60366684+allenzohrabians@users.noreply.github.com>
2020-05-03 14:09:32 -0700

    Update page_offset_calculation.c

--- Lab 5/page_offset_calculation.c
+++ Lab 5/page_offset_calculation.c
@@ -3,26 +3,26 @@
 
 int main(int argc, char **argv)
 {
-  const int PAGE_SIZE = 4096;   // Assume that a system has a 32-bit virtual address with a 4-KB page size
+  const int PAGE_SIZE = 4096;   // 4-KB page size (stored as a constant integer)
   unsigned int address;
   unsigned int page_number;
-  unsigned int offset;
+  unsigned int offset;      // In 32-bit integers, an unsigned integer has a range of 0 to 2^(32)-1 or about 4 billion
 
-  if(argc < 2)
+  if(argc != 2)
   {
     printf("Invalid number of arguments.\n");
-    return -1;
+    return -1;      // The only arguments for the command line are the file name and the decimal virtual address
   }
 
-  address = atoi(argv[1]);
+  address = atoi(argv[1]);    // converts a string argument str to an integer (type int)
 
   page_number = address / PAGE_SIZE;
 
   offset = address % PAGE_SIZE;
 
   printf("The address %d contains: \n", address);
-  printf("Page number = %d \n", page_number);
-  printf("Offset = %d \n", offset);
+  printf("page number = %d \n", page_number);
+  printf("offset = %d \n", offset);
 
   return 0;
 }
