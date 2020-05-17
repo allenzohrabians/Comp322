361eecf0a8beecf15f915fc57dbaf36f47f142ca
allenzohrabians <60366684+allenzohrabians@users.noreply.github.com>
2020-05-17 05:32:30 -0700

    Update matrix_gen.c

--- Lab 7/matrix_gen.c
+++ Lab 7/matrix_gen.c
@@ -4,13 +4,14 @@
 
 int main(int argc, char **argv)
 {
-  srand(time(NULL));
+  srand(time(NULL));    // makes use of the computer's internal clock to control the choice of the seed
 
-  int size = atoi(argv[1]);
+  int size = atoi(argv[1]);   // atoi - converts the string argument to an integer
 
   for(int i = 0; i < size*size; i++)
   {
     int randNum = (rand() % 201) - 100;
     printf("%-5d", randNum);
+    // Generates a matrix of size “size * size” that contains a random set of integers from -100 to +100
   }
 }
