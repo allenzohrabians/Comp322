18d6556d25c6e4693b28f0bf1b3b14a9600dc683
allenzohrabians <60366684+allenzohrabians@users.noreply.github.com>
2020-05-17 02:59:05 -0700

    Update spiritd.c

--- Lab 6/spiritd.c
+++ Lab 6/spiritd.c
@@ -1,24 +1,24 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <signal.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <time.h>
+#include <unistd.h>         // chdir, dup, execve, getcwd
+#include <sys/types.h>      // umask, fork, setsid, kill
+#include <sys/stat.h>       // umask
+#include <sys/time.h>       // getrlimit
+#include <sys/resource.h>   // getrlimit
+#include <signal.h>         // signal, kill
+#include <errno.h>          // errno - number of last error
+#include <stdlib.h>         // rand, srand
+#include <fcntl.h>          // For O_* constants
+#include <stdio.h>          // sprintf, printf, fprintf
+#include <time.h>           // time
 
 int sigtermReceived;
 int sigusrValue;
 
-void handler1(int sig)
-{
+void handler1(int sig)    // Separate handler functions:
+{                         // handler1 for SIGTERM
   sigtermReceived = 1;
 }
 
-void handler2(int sig)
+void handler2(int sig)    // handler2 for SIGUSR1 and SIGUSR2
 {
   if(sig == SIGUSR1)
   {
@@ -29,7 +29,7 @@ void handler2(int sig)
     sigusrValue = 2;
   }
 
-  signal(sig, handler2);
+  signal(sig, handler2);    // the handler registers itself again
 }
 
 int main(int argc, char **argv)
@@ -37,63 +37,72 @@ int main(int argc, char **argv)
   pid_t mole1 = 0;
   pid_t mole2 = 0;
 
-  umask(0);
+  umask(0); // set the file creation mask to 0;
   char buf[50];
   char pathToMole[50];
   getcwd(buf, 50);
   sprintf(pathToMole, "%s/mole", buf);
   printf("%s\n", pathToMole);
-  pid_t process_id = fork();
+  pid_t process_id = fork();  /*  Creates a new process by duplicating the calling process.
+                                  The new process is referred to as the child process.
+                                  The calling process is referred to as the parent process. */
   if(process_id == -1)
   {
     fprintf(stderr, "Can't fork, error %d\n", errno);
     exit(EXIT_FAILURE);
   }
-  else if(process_id == 0)
+  else if(process_id == 0)  // Child process
   {
-    pid_t session_id = setsid();
-    chdir("/");
+    pid_t session_id = setsid();  // Creates a session and sets the process group ID
+
+    chdir("/");   /*  changes the current working directory of the calling
+                      process to the directory specified by the character "/"   */
+
     struct rlimit rlim;
-    getrlimit(RLIMIT_NOFILE, &rlim);
+    getrlimit(RLIMIT_NOFILE, &rlim);  /*  getrlimit - system call that gets resource limit
+                                          RLIMIT_NOFILE - specifies a value one greater than the maximum file
+                                                          descriptor number that can be opened by this process   */
     int i = 0;
-    while(i < (int)rlim.rlim_cur)
+    while(i < (int)rlim.rlim_cur)   // specifies the current or soft limit
     {
       close(i);
-      i++;
+      i++;        // Close all unneeded file descriptors
     }
 
     int file_descriptor = open("/dev/null", O_RDWR);
 
     dup(file_descriptor);
-    dup(file_descriptor);
+    dup(file_descriptor);   // Reopen the standard file descriptors to map to "/dev/null"
 
-    signal(SIGTERM, handler1);
+    signal(SIGTERM, handler1);    // Handler function assignments for SIGTERM, SIGUSR1, and SIGUSR2
     signal(SIGUSR1, handler2);
     signal(SIGUSR2, handler2);
 
-    srand(time(NULL));
+    srand(time(NULL));    // Makes use of the computer's internal clock to control the choice of the seed
 
-    while(sigtermReceived != 1)
+    while(sigtermReceived != 1)   // Executes while a SIGTERM has not reached its designated handler
     {
       int randNum;
 
       pause();
 
-      if(sigusrValue == 1)
+      if(sigusrValue == 1)    // SIGUSR1
       {
         if(mole1 > 0)
         {
-          kill(mole1, SIGTERM);
+          kill(mole1, SIGTERM);   // kill child process #1 (mole 1)
         }
 
-        randNum = rand() % 2 + 1;
+        randNum = rand() % 2 + 1;     // Randomly create either mole 1 or mole 2 if it does not already exists
         if(randNum == 1 && mole1 == 0)
         {
           mole1 = fork();
           if(mole1 == 0)
           {
-            char *args[3] = {"mole1", getenv("HOME"), NULL};
-            execve(pathToMole, args, NULL);
+            char *args[3] = {"mole1", getenv("HOME"), NULL};   /* getenv -  searches the environment list to find the environment variable "HOME",
+                                                                            and returns a pointer to the corresponding value string    */
+
+            execve(pathToMole, args, NULL);   // execve - executes the program referred to by pathToMole
           }
         }
         else if (randNum == 2 && mole2 == 0)
@@ -109,14 +118,14 @@ int main(int argc, char **argv)
         sigusrValue = 0;
       }
 
-      else if(sigusrValue == 2)
+      else if(sigusrValue == 2)   // SIGUSR2
       {
         if(mole2 > 0)
         {
-          kill(mole2, SIGTERM);
+          kill(mole2, SIGTERM);   // kill child process #1 (mole 2)
         }
 
-        randNum = rand() % 2 + 1;
+        randNum = rand() % 2 + 1;   // Randomly create either mole 1 or mole 2 if it does not already exists
         if(randNum == 1 && mole1 == 0)
         {
           mole1 = fork();
@@ -132,13 +141,16 @@ int main(int argc, char **argv)
           if(mole2 == 0)
           {
             char *args[3] = {"mole2", getenv("HOME"), NULL};
-            execve(pathToMole, args, NULL);
+            execve(pathToMole, args, NULL);   // exec the program mole, with the value of argv[0] set to either mole1 or mole2
+
           }
         }
 
         sigusrValue = 0;
       }
     }
+
+    // Upon SIGTERM, the program kills all child processes and shuts down the daemon gracefully
 
     kill(mole1, SIGTERM);
     kill(mole2, SIGTERM);
@@ -148,7 +160,7 @@ int main(int argc, char **argv)
   else
   {
     printf("The process ID is %d\n", process_id);
-    exit(0);
+    exit(0);    // the parent exits after printing out the PID
   }
 
 }
