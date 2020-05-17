2e8fe96759a6effae8dfad045bdba3ec1b8eed72
allenzohrabians <60366684+allenzohrabians@users.noreply.github.com>
2020-05-17 02:56:57 -0700

    Update mole.c

--- Lab 6/mole.c
+++ Lab 6/mole.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
+  // This program writes a signal line to the lab6.log file
+
   char path[100];
   sprintf(path, "%s/lab6.log", argv[1]);
   FILE *logName = fopen(path, "w+");
-  fprintf(logName, "Pop %s\n", argv[0]);
+  fprintf(logName, "Pop %s\n", argv[0]);  // prints either "Pop mole1" or "Pop mole2"
   fclose(logName);
 }
